f88b3fdaca66f750b0a0df4a7244ee17f0b76fdb
NazarTax <94718252+NazarTax@users.noreply.github.com>
2021-11-19 16:52:32 -0500

    Update State.java
    added sorting methods and toString

--- COVID Visualization/src/prj5/State.java
+++ COVID Visualization/src/prj5/State.java
@@ -1,6 +1,8 @@
 package prj5;
 
 /**
+ * Defines a State which has fields for the number of cases and deaths for 
+ * each race (6). 
  * 
  * @author kianp
  * @author nazartax
@@ -12,20 +14,20 @@ public class State {
     private int[] caseData;
     private int[] deathData;
     private Races[] races;
-
-    public State(String nme, int[] cData, int[] dData) {
+    
+    public State(String name, int[] cData, int[] dData) {
 
         caseData = cData;
         deathData = dData;
-        name = nme;
+        this.name = name;
         races[0] = Races.WHITE;
-        races[1] = Races.BLACK;
-        races[2] = Races.LATINX;
-        races[3] = Races.ASIAN;
-        races[4] = Races.OTHER;
-    }
+        races[1] = Races.BLACK; 
+        races[2] = Races.LATINX; 
+        races[3] = Races.ASIAN; 
+        races[4] = Races.OTHER; 
+     }
 
-
+    
     public String getName() {
         return name;
     }
@@ -89,5 +91,83 @@ public class State {
             return -1;
         }
     }
+    
+    /**
+     * Selection Sort algorithm to sort the data by races alphabetically. 
+     */
+    public void sortAlpha() {
+        int size = caseData.length; 
 
+        for (int i = 0; i < size - 1; i++) {
+            
+            int minIndex = i;
+            
+            for (int j = i + 1; j < size; j++) { 
+                if (races[j].toString().compareTo(races[minIndex].toString())
+                    < 0) {
+                    minIndex = j;
+                }
+            }
+
+            Races tempRace = races[minIndex]; 
+            races[minIndex] = races[i]; 
+            races[i] = tempRace; 
+            
+            
+            int tempDeath = deathData[minIndex];
+            deathData[minIndex] = deathData[i];
+            deathData[i] = tempDeath;
+            
+            int tempCase = caseData[minIndex];
+            caseData[minIndex] = caseData[i];
+            caseData[i] = tempCase;
+        }
+    }
+    
+    /**
+     * Selection Sort algorithm to sort the data by CFR from descending order. 
+     */
+    public void sortByCFR() {
+        int size = caseData.length; 
+
+        for (int i = 0; i < size - 1; i++) {
+            
+            int minIndex = i;
+            
+            for (int j = i + 1; j < size; j++) { 
+                if (calculateCFR(races[j]) < calculateCFR(races[minIndex])) {
+                    minIndex = j;
+                }
+            }
+
+            Races tempRace = races[minIndex]; 
+            races[minIndex] = races[i]; 
+            races[i] = tempRace; 
+            
+            
+            int tempDeath = deathData[minIndex];
+            deathData[minIndex] = deathData[i];
+            deathData[i] = tempDeath;
+            
+            int tempCase = caseData[minIndex];
+            caseData[minIndex] = caseData[i];
+            caseData[i] = tempCase;
+        }
+    }
+    
+    /**
+     * Returns a string representation of a State
+     */
+    public String toString() { 
+        StringBuilder builder = new StringBuilder(); 
+        
+        for (int i = 0; i < caseData.length; i++) { 
+            builder.append(races[i].toString() + ": "); 
+            builder.append(caseData[i] + "cases, "); 
+            builder.append(calculateCFR(races[i]) + "% CFR");
+            builder.append("\n"); 
+        }
+        
+        return builder.toString(); 
+    }
 }
