8daa234d4271d96d2f4a3fd2e18fc53976cc6251
Kian Pierce <kianp20@vt.edu>
2021-11-19 12:27:00 -0500

    Created class skeletons.

---
+++ COVID Visualization/src/prj5/GUI.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package prj5;
+
+/**
+ * @author kianp
+ *
+ */
+public class GUI {
+
+}
---
+++ COVID Visualization/src/prj5/Input.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prj5;
+
+/**
+ * @author kianp
+ *
+ */
+public class Input {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
