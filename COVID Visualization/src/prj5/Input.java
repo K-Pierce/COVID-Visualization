11f078a650f1dacf1ea16306e430ebfbde86f98a
NazarTax <94718252+NazarTax@users.noreply.github.com>
2021-11-19 17:01:30 -0500

    updated Input class
    added everything

--- COVID Visualization/src/prj5/Input.java
+++ COVID Visualization/src/prj5/Input.java
@@ -1,19 +1,35 @@
-/**
- * 
- */
+
 package prj5;
+import java.io.File; 
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 /**
+ * Main class to run the project, reads in data from command line input file
+ * name and creates a Linked list to store in the data of each State
+ * and its cases. Prints out State data. 
  * @author kianp
- *
+ * @author nazartax
+ * @version 11/19/2021
  */
 public class Input {
 
     /**
-     * @param args
+     * @param args Command Line inputs
+     * @throws FileNotFoundException if the file does not exist 
      */
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        DataReader reader = new DataReader(args[0]); 
+        SinglyLinkedList<State> allStates = reader.getStates(); 
+        for (int i = 0; i < allStates.size(); i++) { 
+            State currentState = allStates.get(i); 
+            System.out.println(currentState.getName()); 
+            currentState.sortAlpha();
+            System.out.println(currentState.toString()); 
+            System.out.println("===="); 
+            currentState.sortByCFR();
+            System.out.println(currentState.toString()); 
+        }
     }
 
 }
