c6ee79488d8084c2f7a9fe87f7cf822d20d6ddab
Kian Pierce <kianp20@vt.edu>
2021-11-19 13:13:02 -0500

    Coded the States class, added a Races Enum class, created the StateTest and SinglyLnkedListTest classes

--- COVID Visualization/src/prj5/State.java
+++ COVID Visualization/src/prj5/State.java
@@ -1,5 +1,81 @@
 package prj5;
 
+/**
+ * 
+ * @author kianp
+ * @version 19.11.21
+ */
 public class State {
+
+    private String name;
+    private int[] caseData;
+    private int[] deathData;
+
+    public State(String nme, int[] cData, int[] dData) {
+
+        caseData = cData;
+        deathData = dData;
+        name = nme;
+    }
+
+
+    public int getDeathsData(Races race) {
+        switch (race) {
+            case WHITE:
+                return deathData[0];
+            case BLACK:
+                return deathData[1];
+            case LATINX:
+                return deathData[2];
+            case ASIAN:
+                return deathData[3];
+            default:
+                return deathData[4];
+        }
+    }
+
+
+    public int getCaseData(Races race) {
+        switch (race) {
+            case WHITE:
+                return caseData[0];
+            case BLACK:
+                return caseData[1];
+            case LATINX:
+                return caseData[2];
+            case ASIAN:
+                return caseData[3];
+            default:
+                return caseData[4];
+        }
+    }
+
+
+    public double getCFRData(Races race) {
+        switch (race) {
+            case WHITE:
+                return calculateCFR(race);
+            case BLACK:
+                return calculateCFR(race);
+            case LATINX:
+                return calculateCFR(race);
+            case ASIAN:
+                return calculateCFR(race);
+            default:
+                return calculateCFR(race);
+        }
+    }
+
+
+    private double calculateCFR(Races race) {
+
+        if (getDeathsData(race) > 0 && getCaseData(race) > 0) {
+            float div = ((float)getDeathsData(race)) / getCaseData(race);
+            return div * 100;
+        }
+        else {
+            return -1;
+        }
+    }
 
 }
