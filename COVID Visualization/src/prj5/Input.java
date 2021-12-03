f5083f4d03f37f5987fdcff4cd9e4791f3294962
Kian Pierce <kianp20@vt.edu>
2021-12-03 18:30:16 -0500

    Deleted DataContoller Class

--- COVID Visualization/src/prj5/Input.java
+++ COVID Visualization/src/prj5/Input.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 
 /**
  * Main class to run the project, reads in data from command line input file
- * or manually coded file name and passes it to a controller 
+ * or manually coded file name and passes it to a controller
  * and then displaying a window of the data for COVID visualization
  * 
  * @author kianp
@@ -16,9 +16,9 @@ import java.io.FileNotFoundException;
 public class Input {
 
     /**
-     * Uses DataReader to take in data from the file provided, then uses 
-     * DataController to manage the data and create a Linked List from it, 
-     * finally everything is displayed with the GUI class. 
+     * Uses DataReader to take in data from the file provided, then uses
+     * DataController to manage the data and create a Linked List from it,
+     * finally everything is displayed with the GUI class.
      * 
      * @param args
      *            Command Line inputs
@@ -26,7 +26,6 @@ public class Input {
      *             if the file does not exist
      */
     public static void main(String[] args) throws FileNotFoundException {
-<<<<<<< HEAD
 
         DataReader reader;
         if (args.length > 0) {
@@ -50,30 +49,39 @@ public class Input {
             System.out.println(currentState.toString());
             System.out.println("====");
         }
-=======
-        
-        DataReader reader = new DataReader("Cases_and_Deaths_CRDT_NEW.csv"); 
-        DataController controller = new DataController(reader); 
-        GUI newWindow = new GUI(controller); 
-        
+
+        /*
+         * DataReader reader;
+         * if (args.length > 0) {
+         * reader = new DataReader(args[0]);
+         * }
+         * else {
+         * 
+         * reader = new DataReader(
+         * "Cases_and_Deaths_by_race_CRDT_Sep2020.csv");
+         * }
+         * 
+         * DataController controller = new DataController(reader);
+         * GUI newWindow = new GUI(controller);
+         */
+
         /**
-         * (Intermediate Submission) 
+         * (Intermediate Submission)
          * 
          * DataReader reader = new DataReader(args[0]);
          * SinglyLinkedList<State> allStates = reader.getStates();
          * for (int i = 0; i < allStates.size(); i++) {
-         *    State currentState = allStates.get(i);
-         *    System.out.println(currentState.getName());
-         *    currentState.sortAlpha();
-         *    System.out.println(currentState.toString());
-         *    System.out.println("====");
-         *    currentState.sortByCFR();
-         *    System.out.println(currentState.toString());
-         *    System.out.println("====");
+         * State currentState = allStates.get(i);
+         * System.out.println(currentState.getName());
+         * currentState.sortAlpha();
+         * System.out.println(currentState.toString());
+         * System.out.println("====");
+         * currentState.sortByCFR();
+         * System.out.println(currentState.toString());
+         * System.out.println("====");
          * }
          * 
          */
->>>>>>> branch 'main' of https://github.com/K-Pierce/COVID-Visualization
     }
 
 }
