e0c259eef6fbe71f79d68b33f50888a28b41d3d3
Seemill <81532949+Seemill@users.noreply.github.com>
2021-12-03 14:57:27 -0500

    Update GUI.java

--- COVID Visualization/src/prj5/GUI.java
+++ COVID Visualization/src/prj5/GUI.java
@@ -3,11 +3,16 @@
  */
 package prj5;
 
+import CS2GraphWindowLib;
+import java.text.DecimalFormat;
+
 /**
  * @author kianp
  * @author aniketc2002
  * @version 12/3/2021
  */
 public class GUI {
-
+    private final int BAR_GAP = 0;
+    private final int BAR_HEIGHT = 6;
+  
 }
