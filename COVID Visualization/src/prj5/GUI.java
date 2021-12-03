b6f86e3339a36e971804197760ad1db103cb191a
Seemill <81532949+Seemill@users.noreply.github.com>
2021-12-03 14:58:50 -0500

    Update GUI.java

--- COVID Visualization/src/prj5/GUI.java
+++ COVID Visualization/src/prj5/GUI.java
@@ -14,5 +14,31 @@ import java.text.DecimalFormat;
 public class GUI {
     private final int BAR_GAP = 0;
     private final int BAR_HEIGHT = 6;
-  
+    
+    /**
+     * Takes care of making a thread.
+     * Pasuses between disk movement.
+     */
+    private void sleep() {
+        try {
+            Thread.sleep(500);
+        }
+        catch (Exception e) {
+        }
+    } // end sleep
+
+    /**
+     * Calls the puzzle's solve method.
+     * Supports the the solve method. 
+     * 
+     * @param button The button to solve the game. 
+     */
+    public void clickedSolve(Button button) {
+        button.disable();
+        new Thread() {
+            public void run() {
+                game.solve();
+            }
+        }.start();
+    }  
 }
